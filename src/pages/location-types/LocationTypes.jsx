4a830f9ae4067fa9077f0981d91d7b6ddeb7b7ce
AhmedSherif2002 <ahmedsherif602@gmail.com>
2025-10-21 23:57:28 +0300

    feat: add view mode for location types

--- src/pages/location-types/LocationTypes.jsx
+++ src/pages/location-types/LocationTypes.jsx
@@ -10,6 +10,7 @@ import DeleteLocationType from './DeleteLocationType';
 import useLocale from '@/utils/hooks/global/useLocale';
 import i18next from 'i18next';
 import { getOriginalObject } from '@/utils/helpers/global.fns';
+import ViewLocationType from './ViewLocationTypes';
 
 export default function LocationTypes() {
     const { isOpen, toggle } = useIsOpen();
@@ -41,6 +42,12 @@ export default function LocationTypes() {
                 <EditLocationType
                     onClose={toggle.edit}
                     oldData={getOriginalObject(isOpen.edit, data?.data)}
+                />
+            )}
+            {isOpen.view && (
+                <ViewLocationType
+                    onClose={toggle.view}
+                    oldData={getOriginalObject(isOpen.view, data?.data)}
                 />
             )}
             {isOpen.delete && (
