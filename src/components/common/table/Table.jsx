274a7808379fe3fc13a562dd6e6e848d6a251a46
Mohammad Hafez <mhafez296@gmail.com>
2025-12-11 19:34:26 +0200

    style table action buttons

--- src/components/common/table/Table.jsx
+++ src/components/common/table/Table.jsx
@@ -928,7 +928,7 @@ const Table = ({
                                         <button
                                             onClick={handleSaveOrder}
                                             disabled={isSavingOrder}
-                                            className="flex items-center gap-2 px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors disabled:opacity-50"
+                                            className="px-3 py-1 w-fit flex items-center jystify-center gap-2 text-xs bg-primary-500 text-white rounded hover:bg-primary-700 disabled:opacity-50 disabled:cursor-not-allowed"
                                         >
                                             <MdSave className="w-4 h-4" />
                                             {t('common.save')}
@@ -936,7 +936,7 @@ const Table = ({
                                         <button
                                             onClick={handleCancelOrder}
                                             disabled={isSavingOrder}
-                                            className="flex items-center gap-2 px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-colors disabled:opacity-50"
+                                            className="px-3 py-1 w-fit flex items-center jystify-center gap-2 text-xs bg-gray-600 text-white rounded hover:bg-gray-700 disabled:opacity-50 disabled:cursor-not-allowed"
                                         >
                                             <MdCancel className="w-4 h-4" />
                                             {t('common.cancel')}
